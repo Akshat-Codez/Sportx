975770b6cd76857e4a76e5c93417de44003c75e8
akshatshukla25 <akshatshukla.ai25@bmsce.ac.in>
2026-05-30 17:59:44 +0530

    Products

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.static(__dirname));
 // Using pure JS arrays guarantees 100% uptime on Vercel for the prototype.
 
 let products = [
-  { id: 1, tag: "FIFA APPROVED", name: "Pro Football", price: 1499, rent: 39, icon: "⚽", filter: "balls", stock: 24 },
-  { id: 2, tag: "LIGHTWEIGHT", name: "Yonex Nanoflare", price: 5699, rent: 125, icon: "🏸", filter: "rackets", stock: 8 },
-  { id: 3, tag: "CLASSIC", name: "SS Kashmir Willow", price: 2999, rent: 85, icon: "🏏", filter: "sticks", stock: 15 },
-  { id: 4, tag: "DURABLE", name: "Kookaburra Helmet", price: 1299, rent: 49, icon: "⛑️", filter: "helmets", stock: 20 },
-  { id: 5, tag: "SPEED", name: "Nike Mercurial", price: 4999, rent: 190, icon: "👟", filter: "shoes", stock: 12 },
-  { id: 6, tag: "PRO BUNDLE", name: "Full Cricket Kit", price: 12599, rent: 499, icon: "🏏", filter: "kits", stock: 5 },
-  { id: 7, tag: "NBA GRADE", name: "NBA Spalding", price: 3999, rent: 99, icon: "🏀", filter: "balls", stock: 18 },
-  { id: 8, tag: "TOUR", name: "Wilson Pro Staff", price: 18999, rent: 450, icon: "🎾", filter: "rackets", stock: 3 }
+  { id: 1, tag: "FIFA APPROVED", name: "Pro Football", price: 1499, rent: 39, icon: "<img src='https://tse1.mm.bing.net/th/id/OIP.O1PTKf1sohkHbWocvy082gHaE8?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'style='height: 100px;'>", filter: "balls", stock: 24 },
+  { id: 2, tag: "LIGHTWEIGHT", name: "Yonex Nanoflare", price: 2699, rent: 125, icon: "<img src='https://m.media-amazon.com/images/I/61JZ9-KXGEL._AC_.jpg' style='height: 100px;'>", filter: "rackets", stock: 8 },
+  { id: 3, tag: "CLASSIC", name: "SS Kashmir Willow", price: 3999, rent: 105, icon: "<img src='https://nagrathsports.com/wp-content/uploads/2023/07/Super-Select-2-for-web-copy-scaled-1.jpg' style='height: 100px;'>", filter: "sticks", stock: 15 },
+  { id: 4, tag: "DURABLE", name: "Kookaburra Helmet", price: 1299, rent: 49, icon: "<img src='https://cdn.shopify.com/s/files/1/1348/4519/products/kookaburra-cricket-helmet-kb-pro-400-204024_1024x1024.jpg?v=1659218771' style='height: 100px;'>", filter: "helmets", stock: 20 },
+  { id: 5, tag: "SPEED", name: "Nike Mercurial", price: 4999, rent: 190, icon: "<img src='https://thumblr.uniid.it/product/408403/5d2f96677449.jpg?width=750&format=webp&q=75' style='height: 100px;'>", filter: "shoes", stock: 12 },
+  { id: 6, tag: "PRO BUNDLE", name: "Full Cricket Kit", price: 8599, rent: 499, icon: "<img src='https://m.media-amazon.com/images/I/71i6rvL0SaL._SL1200_.jpg' style='height: 100px;'>", filter: "kits", stock: 5 },
+  { id: 7, tag: "NBA GRADE", name: "NBA Spalding", price: 3999, rent: 99, icon: "<img src='https://s.yimg.com/ny/api/res/1.2/3oRP_eLdK8lySncePuZCGw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-images/2020-05/b9b27b40-9547-11ea-b5fb-0e86f7a8e4ad' style='height: 100px;'>", filter: "balls", stock: 18 },
+  { id: 8, tag: "TOUR", name: "Wilson Pro Staff", price: 9999, rent: 550, icon: "<img src='https://tse4.mm.bing.net/th/id/OIP.mSSLeWlmAUk2978hlIUfJAHaE7?r=0&rs=1&pid=ImgDetMain&o=7&rm=3' style='height: 100px;'>", filter: "rackets", stock: 3 }
 ];
 
 let cart = []; // { id, productId, type, qty, days }
